eb6a3964176af21ca0a8ae14ecbb69bafc51f425
Rails Application <shahinatorcse@gmail.com>
2019-12-08 13:40:46 +0600

    Article Edit and Update Complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,31 @@ class ArticlesController < ApplicationController
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        if @article.save
+            flash[:notice] ="Article was Successfully Created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
 
+    def update 
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article wasSuccessfully Updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit 
+        @article = Article.find(params[:id])
+    end
 
 
 
