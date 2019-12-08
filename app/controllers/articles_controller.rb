6d93012b671add882c6d126258f129e3a4a72cfd
Rails Application <shahinatorcse@gmail.com>
2019-12-08 16:06:39 +0600

    Extract Redundency and Prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
 
     def index
         @articles = Article.all
@@ -10,7 +11,6 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        #render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] ="Article was Successfully Created"
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
     end
 
     def update 
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             flash[:notice] = "Article wasSuccessfully Updated"
             redirect_to article_path(@article)
@@ -31,22 +31,26 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
+        
     end
     def destroy
-        @article = Article.find(params[:id])
+        
         @article.destroy
         flash[:notice] ="Article Successfuly Deleted"
         redirect_to articles_path
     end
 
     def edit 
-        @article = Article.find(params[:id])
+        
     end
 
 
 
     private
+        def set_article
+            @article = Article.find(params[:id])
+        end
+
         def article_params
             params.require(:article).permit(:title, :description)
         end
