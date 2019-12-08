b0acc274089abb80f5990ee99ab420e3b2ca6ccb
Rails Application <shahinatorcse@gmail.com>
2019-12-08 23:44:11 +0600

    Style Error Message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] ="Article was Successfully Created"
+            flash[:success] ="Article was Successfully Created"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
     def update 
         
         if @article.update(article_params)
-            flash[:notice] = "Article wasSuccessfully Updated"
+            flash[:success] = "Article wasSuccessfully Updated"
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
     def destroy
         
         @article.destroy
-        flash[:notice] ="Article Successfuly Deleted"
+        flash[:danger] ="Article Successfuly Deleted"
         redirect_to articles_path
     end
 
