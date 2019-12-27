f327c43292746198dcdff60b702d07d85f936ab4
Rails Application <shahinatorcse@gmail.com>
2019-12-27 11:13:39 +0600

    Re-start My self

--- Gemfile
+++ Gemfile
@@ -7,8 +7,8 @@ ruby '2.6.3'
 gem 'rails', '~> 5.2.4'
 # Use sqlite3 as the database for Active Record
 gem 'bcrypt', '~> 3.1.7'
-gem 'will_paginate', '3.1.5'
-gem 'bootstrap-will_paginate', '1.0.0'
+gem 'will_paginate'
+#gem 'bootstrap-will_paginate', '1.0.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -47,6 +47,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
+
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
     def index
         @users = User.all
     end
@@ -37,6 +38,13 @@ class UsersController < ApplicationController
         
     end
 
+    def destroy 
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:danger] ="User and All Artices Have been Deleted"
+        redirect_to users_path
+    end
+
     private
         def user_params
             params.require(:user).permit(:username, :email, :password)
@@ -45,8 +53,15 @@ class UsersController < ApplicationController
             @user = User.find(params[:id])
         end
         def require_same_user
-            if current_user != @user 
+            if current_user != @user and !current_user.admin?
                 flash[:danger] = "You can only edit your own account"
+                redirect_to root_path
+            end
+        end
+
+        def require_admin
+            if logged_in? and !current_user.admin?
+                flash[:danger] ="Only Admin User Can perform that action"
                 redirect_to root_path
             end
         end
