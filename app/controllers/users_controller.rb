c39f841d40f6d394100e1a0d592c5147ba3af2f1
Rails Application <shahinatorcse@gmail.com>
2019-12-30 09:45:06 +0600

    User pages fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     before_action :require_same_user, only: [:edit, :update, :destroy]
     before_action :require_admin, only: [:destroy]
     def index
-        @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
     def new
         @user = User.new
